c44fa651e7fe30c8337148f15e5fc2ba4571a3f5
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-28 21:04:29 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3bfd069ae1942b288404b70358088a2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '36d528e0-7dcb-4a80-a406-d3d5a4e16d09'
+};
